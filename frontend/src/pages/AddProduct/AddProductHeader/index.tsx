ba5113618903716e6ba661fa0f1e52e8007fed2b
Navdish <navdishjaggi@gmail.com>
2024-03-22 17:14:31 +0530

    holi

--- frontend/src/pages/AddProduct/AddProductHeader/index.tsx
+++ frontend/src/pages/AddProduct/AddProductHeader/index.tsx
@@ -7,7 +7,7 @@ import { Link } from "react-router-dom"
 const AddProductHeader: FC = () => {
    
     return (
-        <Box display="flex" justifyContent="space-between">
+        <Box display="flex" justifyContent="space-between" sx={{mb:"24px"}}>
             <HeaderBreadcrumb textStyles={{sx:{height:'32px' , width:'183px'}, fontSize:"24px", fontWeight:"600", lineHeight:"28.44px"}} text={'Product Details'} breadcrumb = {[{text: 'Home', link : '/'}, {text: 'All Products', link: '/all_products'}]}/>
         </Box>
     )
